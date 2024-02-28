4aee1047e4c137424e822efce56c750264f283b5
Keshav Jha <jhak78022@gmail.com>
2024-02-29 00:02:39 +0530

    Loader component done

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,9 +1,32 @@
-const Loader = () => {
-  return (
-    <div>
-      Loader
-    </div>
-  )
-}
+import React from "react";
+import { Html, useProgress } from "@react-three/drei";
 
-export default Loader
+const CanvasLoader: React.FC = () => {
+  const { progress }: { progress: number } = useProgress();
+  return (
+    <Html
+      as="div"
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <span className="canvas-loader"></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: "#F1F1F1",
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      >
+        {progress.toFixed(2)}%
+      </p>
+    </Html>
+  );
+};
+
+export default CanvasLoader;
