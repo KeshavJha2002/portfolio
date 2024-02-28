2a629d6c5e73af6c7fab7ab49ad8a9ba786104c3
Keshav Jha <jhak78022@gmail.com>
2024-02-28 23:58:25 +0530

    Footer done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,17 @@
 const Footer = () => {
-  return (
-    <div>
-      Footer
-    </div>
-  )
-}
 
-export default Footer
+    return (
+        <div className="relative b-[2rem] pb-[4rem] text-center mobile">
+        <p> 
+          &#x3c;&#47;&#x3e; with ❤️ by
+          <a href="https://github.com/KeshavJha2002/portfolio" target="_blank" className="text-[#6E6EF0] no-underline">
+            {" "}
+            Keshav Jha
+          </a>
+          😎
+        </p>
+      </div>
+    );
+  };
+  
+  export default Footer;
