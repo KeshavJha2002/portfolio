e1a6264dbdf2a042f16a5c0ab74faaee26cf8f4b
Keshav Jha <jhak78022@gmail.com>
2024-03-01 19:28:37 +0530

    styles

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -9,7 +9,7 @@ type ProjectListProp = {
 
 const  ProjectList:React.FC<ProjectListProp> = ({title,active,setSelected,id}) => {
     return (
-        <li className={`text-[14px] p-[7px] my-[0px] mx-[23px] font-bold rounded-[10px] cursor:pointer overflow-hidden justify-center items-center width1:m-auto ${active?"bg-[#398485] text-white":""}`} id="portfoliolist" onClick={() => setSelected(id)}>
+        <li className={`border-[2px] border-[#00e9ff] text-[14px] p-[7px] my-[0px] mx-[23px] font-bold rounded-[10px] cursor:pointer overflow-hidden justify-center items-center width1:m-auto ${active?"bg-[#398485] text-white":""}`} id="portfoliolist" onClick={() => setSelected(id)}>
         {title}
       </li>
     )
