2d0950ecf0a37aaa079613f1a79aeba38ff72053
Keshav Jha <jhak78022@gmail.com>
2024-03-01 17:31:29 +0530

    Tech component added

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,9 +1,30 @@
-const Tech = () => {
-  return (
-    <div>
-      Tech
-    </div>
-  )
-}
+import React from "react";
 
-export default Tech
+import { BallCanvas } from "./canvas";
+import SectionWrapper from "../hoc/SectionWrapper";
+import { technologies } from "../constants";
+import { textVariant } from "../utils/motion";
+import { motion, Variants } from "framer-motion";
+
+// TODO -> check the delay passed to the textVariant() and make updates in all the components that are using it
+
+const Tech:React.FC = () => {
+  return (
+    <>
+      <motion.div id="tech" variants={textVariant(200) as Variants}>
+        <h2 className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px] text-center" >
+          Tools
+        </h2>
+      </motion.div>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology) => (
+        <div style={{height: '5rem', width: "5rem"}} key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Tech, "");
