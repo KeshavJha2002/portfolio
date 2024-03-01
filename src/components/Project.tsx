2a7a4477f70f9672a4311672cc1744582869d031
Keshav Jha <jhak78022@gmail.com>
2024-03-01 18:40:26 +0530

    refractor: style

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -47,18 +47,18 @@ const ProjectCard: React.FC<ProjectData> = ({
           scale: 1,
           speed: 450,
         }}
-        className="project-box bg-tertiary p-5 rounded-2xl sm:w-[330px] w-full"
+        className="h-[500px] bg-tertiary p-5 rounded-2xl sm:w-[330px] w-full"
       >
-        <div className="Box1 relative w-full h-[180px]">
+        <div className="cursor-pointer relative w-full h-[180px] hover:bg-[#030325]">
           <img
             src={image}
             alt="project_image"
-            className="image w-full h-full object-cover rounded-2xl"
+            className="z-[1] hover:z-[2] hover:opacity-[0.2] w-full h-full object-cover rounded-2xl"
           />
           <div className="absolute inset-0 flex justify-center card-img_hover" style={{ alignItems: "center" }}>
-            <h3 className="text-black font-bold text-[16px]">{name}</h3>
+            <h3 className="opacity-0 text-black font-bold text-[16px] hover:z-[1] hover:opacity-[1] hover:text-[#fff]">{name}</h3>
           </div>
-          <div className="title absolute inset-0 flex justify-end card-img_hover">
+          <div className="hover:opacity-[1] hover:z-[1] opacity-0 absolute inset-0 flex justify-end card-img_hover">
             <div
               onClick={() => window.open(source_link, "_blank")}
               className="black-gradient w-10 h-10 m-2 rounded-full flex justify-center items-center cursor-pointer"
@@ -125,13 +125,13 @@ const Project: React.FC = () => {
         <p className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider">My work</p>
         <h2 className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">Projects.</h2>
       </motion.div>
-      <div className="project w-full flex">
+      <div className="flex flex-col items-center w-full">
         <motion.p
           whileInView={{ opacity: 1, transform: 'none' }}
           variants={fadeIn("left", "", 0.1, 1) as Variants}
           className="mt-3 text-secondary text-[17px] leading-[30px]"
         >
-          <ul>
+          <ul className="p-0 m-[10px] flex justify-center list-none">
             {list.map((item) => (
               <ProjectList
                 key={item.id}
@@ -142,7 +142,7 @@ const Project: React.FC = () => {
               />
             ))}
           </ul>
-          <div className="box mt-20 flex flex-wrap justify-center">
+          <div className="gap-[1.75rem] mt-20 flex flex-wrap justify-center">
             {data.map((project, index) => (
               <div key={`project-${index}`}>
                 <ProjectCard {...project} index={index} />
